048810871f0a90244be8b23d0cd49bcdb4f4f579
nov144 <nov144@mail.ru>
2025-06-05 15:53:13 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,3 @@ from config import TELEGRAM_BOT_TOKEN
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dp = Dispatcher()
-
-from aiogram import types
-from aiogram.filters import Command
-
-@dp.message(Command("start"))
-async def handle_start(message: types.Message):
-    await message.answer("Привет!")
